657d93a81f9e4a071b580e1e97e537360306e924
kaelacodes <120235236+kaelacodes@users.noreply.github.com>
2023-01-30 09:52:42 -0800

    Adds listener for port 8080, and changes some order of operations in syntax

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 // Imports all required packages
 const express = require('express'),
     bodyParser = require('body-parser'),
+    morgan = require('morgan'),
     fs = require('fs'),
-    path = require('path'),
-    morgan = require('morgan');
+    path = require('path');
 
 const app = express();
-
-const accessLogStream = fs.createWriteStream(path.join(_dirname,'log.txt'), {flags:'a'});
 
 let topTenMovies = [
     {title: 'Happy Gilmore'},
@@ -19,24 +17,32 @@ let topTenMovies = [
     {title: 'Pulp Fiction'},
     {title: 'I Am Legend'},
     {title: 'Pretty In Pink'},
-    {title: 'Accross The Universe'}
+    {title: 'Everything, Everywhere, All At Once'}
 ];
+
+const accessLogStream = fs.createWriteStream(path.join(__dirname,'log.txt'), {flags:'a'});
 
 app.use(bodyParser.json());
 app.use(express.static('public'));
-app.use(morgan('combines', {stream: accessLogStream}));
+app.use(morgan('combined', {stream: accessLogStream}));
+
+//GET request for default resposnse at "/" endpoint
+app.get('/', (req, res) => {
+    res.send('Welcome to MyFlix!');
+});
 
 //GET request for topTenMovies
 app.get('/movies', (req,res) => {
     res.json(topTenMovies);
-});
-//GET request for default resposnse at "/" endpoint
-app.get('/', (req,res) => {
-    res.send('Welcome to MyFlix!');
 });
 
 // error handler comes after all route calls and app.use, but before app.listen
 app.use((err,req,res,next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
+});
+
+// listen for requests
+app.listen(8080, () => {
+    console.log('Your app is listening on port 8080.');
 });
