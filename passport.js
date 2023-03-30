3d0c1cfc150b0f0dd90a112301625ac4b4b574ed
kaelacodes <120235236+kaelacodes@users.noreply.github.com>
2023-03-29 18:18:40 -0700

    Spelling fix

--- passport.js
+++ passport.js
@@ -21,7 +21,7 @@ passport.use(new LocalStrategy({
             console.log('incorrect username');
             return callback(null, false, {message: 'Incorrect username or password.'});
         }
-        if (!user.validatePassword(passwrod)) {
+        if (!user.validatePassword(password)) {
             console.log('incorrect password');
             return callback(null, false, {message: 'Incorrect password.'});
         }
