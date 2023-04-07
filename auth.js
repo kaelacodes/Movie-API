a51ddd22ece3e5c4c30e3ac12e729e35143def03
kaelacodes <120235236+kaelacodes@users.noreply.github.com>
2023-04-06 19:55:19 -0700

    syntax edits to login endpoint in auth.js file

--- auth.js
+++ auth.js
@@ -16,19 +16,21 @@ let generateJWTToken = (user) => {
 /* POST login */
 module.exports = (router) => {
     router.post('/login', (req, res) => {
-        passport.authenticate('local', {session:false}, (error, user, info) => {
+        passport.authenticate('local', { session: false }, (error, user, info) => {
+            console.log({error, user})
             if (error || !user) {
                 return res.status(400).json({
                     message: 'Something is not right',
-                    user: user
+                    user: user,
+                    error
                 });
             }
-            req.login(user, {session:false}, (error) => {
+            req.login(user, { session: false }, (error) => {
                 if (error) {
                     res.send(error);
                 }
                 let token = generateJWTToken(user.toJSON());
-                return res.json({user, token});
+                return res.json({ user, token });
             });
         })(req, res);
     });
